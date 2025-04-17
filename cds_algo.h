8328fa040358db7d4f2223c7328c3c7068b511dc
Gustavo Aguiar <theghostassassin09@gmail.com>
2025-04-17 13:25:09 -0400

    Fix thresholds for thin film sensor

--- cds_algo.h
+++ cds_algo.h
@@ -7,9 +7,9 @@ const int ULTRASONIC_SENSOR_THRESHOLD = 50;    // Maximum distance to trigger co
 const int DISTANCE_THRESHOLD = 50;             // If the object is farther than this, we do nothing
 const int DANGER_THRESHOLD = 20;               // If an object is detected under this distance, we check for collision confirmation
 const int COLLISION_DETECTION_THRESHOLD = 0;   // 0 for impact detected, 1 for no impact
-const int THINFILM_SENSOR_THRESHOLD_HIGH = 80; // High severity impact threshold for the thin-film sensor
-const int THINFILM_SENSOR_THRESHOLD_MID = 50;  // Moderate severity threshold for the thin-film sensor
-const int THINFILM_SENSOR_THRESHOLD_LOW = 30;  // Low severity threshold for the thin-film sensor
+const int THINFILM_SENSOR_THRESHOLD_HIGH = 3500; // High severity impact threshold for the thin-film sensor
+const int THINFILM_SENSOR_THRESHOLD_MID = 1500;  // Moderate severity threshold for the thin-film sensor
+const int THINFILM_SENSOR_THRESHOLD_LOW = 500;  // Low severity threshold for the thin-film sensor
 
 // Function declarations for collision detection system
 void precollision_warning(int trigPin, int echoPin, int colPin);
