aaf6f3b356ea6b486bfc8dd48ebf7039142e3d81
Gustavo Aguiar <theghostassassin09@gmail.com>
2025-05-09 09:34:48 -0400

    Clean up and comment code

--- sensor_functions.cpp
+++ sensor_functions.cpp
@@ -3,16 +3,13 @@
 #include <Arduino.h>
 
 double read_ultrasonic_sns(int trigPin, int echoPin) {
-    return 5;
-    //SendDistanceSensorData(trigPin, echoPin);
+    return SendDistanceSensorData(trigPin, echoPin);
 }
 
 int read_collision_sns(int pin) {
-    return 0;
-    //SendCollisionSensorData(pin);
+    return SendCollisionSensorData(pin);
 }
 
 double get_thinfilm_data(int pin) {
-    return 80;
-    //SendPressureSensorData(pin);
+    return SendPressureSensorData(pin);
 }
