a4e6df8a88cfc50a82138aa6a5dd5bef66e41067
Gustavo Aguiar <theghostassassin09@gmail.com>
2025-05-09 09:40:42 -0400

    Add doxygen comments for headers

--- sensor_functions.h
+++ sensor_functions.h
@@ -1,8 +1,26 @@
 #ifndef SENSOR_FUNCTIONS_H
 #define SENSOR_FUNCTIONS_H
 
+/**
+  * @brief Read ultrasonic sensor data
+  * @param trigPin Pin number for trig pin
+  * @param echoPin Pin number for echo pin
+  * @return Distance in cm as a double
+  */
 double read_ultrasonic_sns(int trigPin, int echoPin); 
+
+/**
+  * @brief Read collision sensor data
+  * @param pin Pin number for collision pin
+  * @return 0 if collision detected and 1 otherwise
+  */
 int read_collision_sns(int pin);
+
+/**
+  * @brief Read thin film sensor data
+  * @param pin Pin number for collision pin
+  * @return Read pressure value as an int
+  */
 double get_thinfilm_data(int pin);
 
 #endif
