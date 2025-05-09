5243cb97847a920700c61a17835fe38ca94dc6d4
Gustavo Aguiar <theghostassassin09@gmail.com>
2025-05-09 09:52:33 -0400

    Clean up and comment code

--- cds_algo.cpp
+++ cds_algo.cpp
@@ -1,13 +1,9 @@
-
-
 // include section
 #include "cds_algo.h"
 #include "sensor_functions.h"
 #include <Arduino.h>
 #include "mqtt_comm.h"
 #include <ArduinoJson.h> 
-
-
 
 void trigger_warning(double ultrasonic_dist, String timestamp, MqttComm &mqtt) {
   bool isCollision = false;
@@ -18,6 +14,7 @@ void trigger_warning(double ultrasonic_dist, String timestamp, MqttComm &mqtt) {
 
   // Create a JSON document for the warning
   StaticJsonDocument<256> jsonDoc;
+  jsonDoc["device_id"] = "DEMOESP";
   jsonDoc["timestamp"] = timestamp;
   jsonDoc["collision"] = isCollision;
   jsonDoc["severity"] = "PRE-COLLISION WARNING";
@@ -39,6 +36,7 @@ void trigger_collision_alert(String message, double ultrasonic_dist, double thin
 
   // Create a JSON document for the collision
   StaticJsonDocument<256> jsonDoc;
+  jsonDoc["device_id"] = "DEMOESP";
   jsonDoc["timestamp"] = timestamp;
   jsonDoc["collision"] = isCollision;
   jsonDoc["severity"] = message;
@@ -51,52 +49,10 @@ void trigger_collision_alert(String message, double ultrasonic_dist, double thin
   // Publish the JSON message to MQTT
   mqtt.publish(jsonPayload); 
 }
-// for pre collision detection only if the object is within the allotted distance will we warn the user
 
-void precollision_warning(int trigPin, int echoPin, int colPin, String timestamp, MqttComm &mqtt)
-{
-  double ultrasonic_dist = read_ultrasonic_sns(trigPin, echoPin);
-  int is_collision_detected = read_collision_sns(colPin);
-  
-
-  if (ultrasonic_dist < DISTANCE_THRESHOLD && ultrasonic_dist >= DANGER_THRESHOLD && is_collision_detected != COLLISION_DETECTION_THRESHOLD)
-  {
-    trigger_warning(ultrasonic_dist, timestamp, mqtt);
-  }
-  if (ultrasonic_dist < DANGER_THRESHOLD && is_collision_detected != COLLISION_DETECTION_THRESHOLD)
-  {
-    trigger_warning(ultrasonic_dist, timestamp, mqtt);
-  }
-}
-
-// Collision Confirmation
-
-void collision_detected(int trigPin, int echoPin, int colPin, int pressurePin, String timestamp, MqttComm &mqtt)
-{
+bool collision_detected(int trigPin, int echoPin, int colPin, int pressurePin) {
   double ultrasonic_dist = read_ultrasonic_sns(trigPin, echoPin);
   int is_collision_detected = read_collision_sns(colPin);
 
-  
-  if (ultrasonic_dist < DANGER_THRESHOLD && is_collision_detected == COLLISION_DETECTION_THRESHOLD)
-  {
-    double thinfilm_severeity = get_thinfilm_data(pressurePin);
-    Serial.print("Thin Film Severity: ");
-    Serial.println(thinfilm_severeity);
-    if (thinfilm_severeity >= THINFILM_SENSOR_THRESHOLD_HIGH)
-    {
-      trigger_collision_alert("HIGH-SEVERITY IMPACT!", ultrasonic_dist, thinfilm_severeity, timestamp, mqtt);
-    }
-    else if (thinfilm_severeity >= THINFILM_SENSOR_THRESHOLD_MID)
-    {
-      trigger_collision_alert("MODERATE-SEVERITY IMPACT!", ultrasonic_dist, thinfilm_severeity, timestamp, mqtt);
-    }
-    else if (thinfilm_severeity >= THINFILM_SENSOR_THRESHOLD_LOW)
-    {
-      trigger_collision_alert("LOW-SEVERITY IMPACT!", ultrasonic_dist, thinfilm_severeity, timestamp, mqtt);
-    }
-    else
-    {
-      trigger_collision_alert("NO SEVERE IMPACT", ultrasonic_dist, thinfilm_severeity, timestamp, mqtt);
-    }
-  }
+  return ultrasonic_dist < DANGER_THRESHOLD && is_collision_detected == COLLISION_DETECTION_THRESHOLD;
 }
