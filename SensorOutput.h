8c8b8fe59ad1d061b4c9b3e7f69ce10419a8f47b
Gustavo Aguiar <theghostassassin09@gmail.com>
2025-05-09 09:36:44 -0400

    Remove wifi code

--- SensorOutput.h
+++ SensorOutput.h
@@ -11,23 +11,6 @@ Description: Contains functions use to read sensor data
 #include <string>
 
 /**
- * @brief Connect to WiFi network
- * @param ssid SSID of the WiFi network
- * @param password Password of the WiFi network
- * @return void
- * @note This function should be called before attempting to connect to the WiFi network.
- */
-//void ConnectWiFi(const char* ssid, const char* password);
-
-/**
- * @brief Continuously check the WiFi connection status and reconnect if necessary
- * @param void
- * @return void
- * @note This function should be called in the main loop to ensure the device stays connected to WiFi.
- */ 
-void WiFiCheck(bool &isConnected);
-
-/**
  * @brief Send pressure sensor data to the server
  * @param pin Pin number of the pressure sensor
  * @return Sensor data as an int
