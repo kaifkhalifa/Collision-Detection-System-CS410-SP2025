9315db2c64ac765fc979e1e8af2135f31d9b9655
Gustavo Aguiar <theghostassassin09@gmail.com>
2025-05-09 09:51:52 -0400

    Clean up and comment code

--- cds_algo.h
+++ cds_algo.h
@@ -6,16 +6,39 @@
 #include "mqtt_comm.h"
 
 const int DISTANCE_THRESHOLD = 50;             // If the object is farther than this, we do nothing
-const int DANGER_THRESHOLD = 20;               // If an object is detected under this distance, we check for collision confirmation
 const int COLLISION_DETECTION_THRESHOLD = 0;   // 0 for impact detected, 1 for no impact
-const int THINFILM_SENSOR_THRESHOLD_HIGH = 80; // High severity impact threshold for the thin-film sensor
-const int THINFILM_SENSOR_THRESHOLD_MID = 50;  // Moderate severity threshold for the thin-film sensor
-const int THINFILM_SENSOR_THRESHOLD_LOW = 30;  // Low severity threshold for the thin-film sensor
+const int THINFILM_SENSOR_THRESHOLD_HIGH = 3500; // High severity impact threshold for the thin-film sensor
+const int THINFILM_SENSOR_THRESHOLD_MID = 1000;  // Moderate severity threshold for the thin-film sensor
+const int THINFILM_SENSOR_THRESHOLD_LOW = 100;  // Low severity threshold for the thin-film sensor
 
-// Function declarations for collision detection system
-void precollision_warning(int trigPin, int echoPin, int colPin, String timestamp, MqttComm &mqtt);
-void collision_detected(int trigPin, int echoPin, int colPin, int pressurePin, String timestamp, MqttComm &mqtt);
-void trigger_warning(String message, MqttComm &mqtt);
-void trigger_collision_alert(String message, MqttComm &mqtt);
+/**
+  * @brief Sends a warning if within the distance threshold
+  * @param trigPin Pin number for trig pin
+  * @param echoPin Pin number for echo pin
+  * @param colPin Pin number for collision pin
+  * @param timestamp Current data and time of when the function was called
+  * @param mqtt MQTT class object reference
+  */
+void trigger_warning(double ultrasonic_dist, String timestamp, MqttComm &mqtt);
+
+/**
+  * @brief Sends a collision warning
+  * @param trigPin Pin number for trig pin
+  * @param echoPin Pin number for echo pin
+  * @param colPin Pin number for collision pin
+  * @param timestamp Current data and time of when the function was called
+  * @param mqtt MQTT class object reference
+  */
+void trigger_collision_alert(String message, double ultrasonic_dist, double thinfilm_severity, String timestamp, MqttComm &mqtt);
+
+/**
+  * @brief Checks if a collision was detected 
+  * @param trigPin Pin number for trig pin
+  * @param echoPin Pin number for echo pin
+  * @param colPin Pin number for collision pin
+  * @param pressurePin Pin number for pressure pin
+  * @return true if collision was detected
+  */
+bool collision_detected(int trigPin, int echoPin, int colPin, int pressurePin);
 
 #endif
