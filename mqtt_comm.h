fc61817f0809c364e231b73d47d8997bf0abc958
emry <45548784+emhankins@users.noreply.github.com>
2025-05-16 23:02:49 -0400

    Update mqtt_comm.h
    added doxygen comments

--- mqtt_comm.h
+++ mqtt_comm.h
@@ -7,9 +7,28 @@
 
 class MqttComm {
   public:
+/**
+     * @brief Constructor for MqttComm.
+     * @param ssid WiFi network SSID.
+     * @param password WiFi network password.
+     * @param mqtt_broker MQTT broker address.
+     * @param mqtt_port MQTT broker port.
+     * @param mqtt_username MQTT broker username.
+     * @param mqtt_password MQTT broker password.
+     * @param mqtt_topic MQTT topic to publish messages to.
+     */
     MqttComm(const char* ssid, const char* password, const char* mqtt_broker, const int mqtt_port, const char *mqtt_username,
             const char* mqtt_password, const char* mqtt_topic);
+
+
+    /**
+     * @brief Initializes WiFi and MQTT connections.
+     */
     void begin();
+
+  /**
+     * @brief keeps the MQTT running.
+     */
     void loop();
     void publish(String jsonPayload);
   
@@ -24,9 +43,23 @@ class MqttComm {
 
     WiFiClient _wifiClient;
     PubSubClient _mqttClient;
-
+    /**
+     * @brief Establishes a connection to the WiFi network.
+     */
     void connectWiFi();
+
+    
+    /**
+     * @brief Connects to the MQTT broker and subscribes.
+     */
     void connectMQTT();
+
+    /**
+     * @brief Static callback function for handling incoming MQTT messages.
+     * @param topic The topic the message was received on.
+     * @param payload The message payload.
+     * @param length The length of the message payload.
+     */
     static void mqttCallback(char* topic, byte* payload, unsigned int length);
 };
 #endif
